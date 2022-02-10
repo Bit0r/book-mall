6e16766cdf1de61155b5b341bfe58cc7d58d837f
Bit0r <nie_wang@outlook.com>
2022-02-10 21:53:26 +0800

    📦 使用 viper 读取配置

--- model/connect.go
+++ model/connect.go
@@ -2,11 +2,40 @@ package model
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 )
 
-var (
-	dsn   = "super:YOUR_PASSWD@tcp(localhost:3306)/StoreOL"
+type config struct {
+	User     string
+	Password string
+	Host     string
+	Port     uint16
+	Name     string
+}
+
+var db *sql.DB
+
+func init() {
+	viper.SetConfigFile("config.toml")
+	viper.AddConfigPath("/etc/book-mall")
+	viper.AddConfigPath("$HOME/.config/book-mall")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	dbConfig := viper.Sub("database")
+
+	var c config
+	dbConfig.Unmarshal(&c)
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v",
+		c.User, c.Password,
+		c.Host, c.Port,
+		c.Name)
 	db, _ = sql.Open("mysql", dsn)
-)
+}
